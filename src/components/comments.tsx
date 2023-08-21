7371828c5b3b954d819d593658fd8a7f5276df57
joy-milexa <115990500+joy-milexa@users.noreply.github.com>
2023-08-21 14:58:37 +0100

    Testing out comments fix

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -3,22 +3,23 @@ import { WEBSITE_URL } from "config";
 export default async function Comments({slug}: {slug:string}) {
     console.log(process.env.API_URL);
 
-    const commentsRes = await fetch(`${WEBSITE_URL}/api/comments/${slug}`, {next: {revalidate: 10}});
-    const comments = await commentsRes.json();
+    // const commentsRes = await fetch(`${WEBSITE_URL}/api/comments/${slug}`, {next: {revalidate: 10}});
+    // const comments = await commentsRes.json();
+    
 
     return (
         <div className="pt-5 pb-5">
             <h1 className="text-2xl pt-3 pb-3">Comments</h1>
 
             {/* @ts-ignore */}
-            {comments.map((comment) => {
+            {/* {comments.map((comment) => {
                 return (
                     <div className="pt-5 pb-5 border-b-2 border-gray-100" key={comment.uuid}>
                         <strong>{comment.username}</strong>
                         <p>{comment.comment}</p>
                     </div>
                 );
-            })}
+            })} */}
 
             <h3 className="text-2xl pt-5">Submit a Comment</h3>
             
