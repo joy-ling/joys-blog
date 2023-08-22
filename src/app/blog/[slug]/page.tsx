74bd570ed242105f5324071df3206b93b8d5dd19
joy-milexa <115990500+joy-milexa@users.noreply.github.com>
2023-08-22 16:00:41 +0100

    Fixed silly image bug

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -26,7 +26,7 @@ export default function PostLayout ({ params }: { params: { slug: string } }) {
 
       <div className="flex flex-col items-center">
         
-        <div className="w-full w-[700px] h-[400px] shadow-md relative">
+        <div className="w-[700px] h-[400px] shadow-md relative">
             <Image  src={post.featuredImage} alt={post.description} layout='fill' objectFit='cover'></Image>
         </div>
 
